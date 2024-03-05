49a3d580106436baf91c063d0530c10831689033
LJD <Lukaa0724@gmail.com>
2024-03-05 22:50:54 +0800

    update

---
+++ Rookie/Data encapsulation/1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+class Box
+{
+    public:
+        double getVolume(void)
+        {
+            return 
+        }
+    
+    private:
+        double length;
+        double breadth;
+        double height;
+}
---
+++ Rookie/ManyStates/1.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+using namespace std;
+
+class Shape
+{
+    protected:
+        int width, height;
+    public:
+        Shape(int a = 0, int b = 0)
+        {
+            width = a;
+            height = b;
+        }
+        virtual int area()
+        {
+            return height * width;
+        }
+};
+
+class Rectangle: public Shape
+{
+    public:
+        Rectangle(int a = 0, int b = 0) : Shape(a, b) {}
+        virtual int area()
+        {
+            cout << "Rectangle class area : " << endl;
+            return width * height;
+        }
+};
+
+class Triangle: public Shape
+{
+    public: 
+        Triangle(int a = 0, int b = 0) : Shape(a, b){}
+        virtual int area()
+        {
+            cout << "Triangle class area : " << endl;
+            return width * height / 2;
+        }
+};
+
+int main()
+{
+    Shape *shape;
+    Rectangle rec(10, 7);
+    Triangle tri(10, 5);
+
+    shape = &rec;
+    shape->area();
+
+    shape = &tri;
+    shape->area();
+
+    return 0;
+}
